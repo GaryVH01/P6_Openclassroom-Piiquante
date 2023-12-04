3df076b170e4345d2db0e1b5a4ccd3640afc67c5
GaryVH <gary.vanhecke@gmail.com>
2023-12-04 17:39:25 +0100

    change localhost to adress of render backend

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, tap } from 'rxjs';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, tap } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
   isAuth$ = new BehaviorSubject<boolean>(false);
-  private authToken = '';
-  private userId = '';
+  private authToken = "";
+  private userId = "";
 
-  constructor(private http: HttpClient,
-              private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   createUser(email: string, password: string) {
-    return this.http.post<{ message: string }>('http://localhost:3000/api/auth/signup', {email: email, password: password});
+    return this.http.post<{ message: string }>(
+      "https://piiquante-backend-wjow.onrender.com/api/auth/signup",
+      { email: email, password: password }
+    );
   }
 
   getToken() {
@@ -28,20 +29,24 @@ export class AuthService {
   }
 
   loginUser(email: string, password: string) {
-    return this.http.post<{ userId: string, token: string }>('http://localhost:3000/api/auth/login', {email: email, password: password}).pipe(
-      tap(({ userId, token }) => {
-        this.userId = userId;
-        this.authToken = token;
-        this.isAuth$.next(true);
-      })
-    );
+    return this.http
+      .post<{ userId: string; token: string }>(
+        "https://piiquante-backend-wjow.onrender.com/api/auth/login",
+        { email: email, password: password }
+      )
+      .pipe(
+        tap(({ userId, token }) => {
+          this.userId = userId;
+          this.authToken = token;
+          this.isAuth$.next(true);
+        })
+      );
   }
 
   logout() {
-    this.authToken = '';
-    this.userId = '';
+    this.authToken = "";
+    this.userId = "";
     this.isAuth$.next(false);
-    this.router.navigate(['login']);
+    this.router.navigate(["login"]);
   }
-
 }
